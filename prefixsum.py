f36a59ba8b4e307b1c108487b757ea73b8892ee8
Alisher Zhubanyshev <saegl@protonmail.com>
2024-01-28 10:00:05 +0600

    OOP prefixsum

--- prefixsum.py
+++ prefixsum.py
@@ -1,18 +1,20 @@
-arr = [3, 2, 4, 1, 5]
+class PrefixSum:
+    def __init__(self, arr: list):
+        self.pref = [0]
+        for n in arr:
+            self.pref.append(self.pref[-1] + n)
 
-# init prefix array
-prefix = [0]
-for n in arr:
-    prefix.append(prefix[-1] + n)
-
-# usage
-i = 1
-j = 4
-print(sum(arr[i:j]) == prefix[j] - prefix[i])
+    def sum(self, i, j):
+        return self.pref[j] - self.pref[i]
 
 
-# test
-for i in range(len(arr)):
-    for j in range(i, len(arr)):
-        assert sum(arr[i:j]) == prefix[j] - prefix[i]
+def test_arr():
+    arr = [3, 2, 4, 1, 5]
+    pref = PrefixSum(arr)
+
+    # sum(arr[i:j]) == pref.sum(i, j)
+
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            assert sum(arr[i:j]) == pref.sum(i, j)
 
